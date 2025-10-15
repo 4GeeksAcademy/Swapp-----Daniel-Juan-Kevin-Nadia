ad1bfc66f1f0ac3231508951903eb5bab57ba03a
Nadia <nadyah-88@hotmail.com>
2025-10-15 21:48:20 +0000

    Crear componente CardUsuario

--- front/App.jsx
+++ front/App.jsx
@@ -2,12 +2,14 @@ import "./assets/styles/App.css";
 import Navbar from "./assets/components/Navbar";
 import Footer from "./assets/components/Footer";
 import Carousel from "./assets/components/Carousel";
+import CardUsuario from "./assets/components/CardUsuario";
 
 export default function App() {
   return (
     <>
       <Navbar></Navbar>
       <Carousel></Carousel>
+      <CardUsuario></CardUsuario>
       <Footer></Footer>
     </>
   );
---
+++ front/assets/components/CardUsuario.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "../styles/CardUsuario.css";
+
+function CardUsuario() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch("https://68e8dfb5f2707e6128cc97d2.mockapi.io/api/usuario")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsuarios(data.slice(0, 6));
+      })
+      .catch((err) => console.error("Error al cargar usuarios:", err));
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid mt-5">
+        <h3 className="text-center">Recomendados</h3>
+        <div className="row mt-5">
+          {usuarios.map((usuario) => (
+            <div className="col-md-4 mb-4" key={usuario.id}>
+              <div className="card  h-100">
+                <div className="card-header d-flex justify-content-between">
+                  <img
+                    src={usuario.foto_perfil || "No hay foto"}
+                    className="card-img-top foto-perfil"
+                    alt={usuario.nombre}
+                  />
+                  <button className="btn btn-main1 px-4" type="submit">
+                    Ver perfil
+                  </button>
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title  text-center">
+                    {usuario.nombre} {usuario.apellidos}
+                  </h5>
+                  <p className="card-text  text-center">
+                    {usuario.descripcion || "Sin descripción"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CardUsuario;
