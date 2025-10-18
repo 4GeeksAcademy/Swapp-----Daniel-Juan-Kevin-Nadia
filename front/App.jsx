92a55e4e779e51a6ea49838c69f1158a0bec3bb9
Daniel-And <danielanduezato@hotmail.com>
2025-10-18 00:52:47 +0000

    Agrego vistas de login y perfil de usuario, conectadas

--- front/App.jsx
+++ front/App.jsx
@@ -1,8 +1,9 @@
 import "./assets/styles/App.css";
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Registro from "./pages/Registro";
+import Login from "./pages/Login";
+import PerfilUsuario from "./pages/PerfilUsuario";
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/perfil" element={<PerfilUsuario />} />
       </Routes>
     </>
   );
--- front/assets/components/Navbar.jsx
+++ front/assets/components/Navbar.jsx
@@ -1,18 +1,40 @@
 import "../styles/Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [usuario, setUsuario] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const usuarioGuardado = localStorage.getItem("usuario");
+    if (usuarioGuardado) {
+      setUsuario(JSON.parse(usuarioGuardado));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    const confirmLogout = window.confirm("¿Deseas cerrar sesión?");
+    if (confirmLogout) {
+      localStorage.removeItem("usuario");
+      setUsuario(null);
+      navigate("/login");
+    }
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary px-3">
-        <div className="container-fluid  align-items-center">
-          <a
-            className="navbar-brand d-flex  align-items-center logo-container"
-            href="#"
+        <div className="container-fluid align-items-center">
+          {/* Logo Swapp (redirige a Home) */}
+          <Link
+            className="navbar-brand d-flex align-items-center logo-container"
+            to="/"
           >
-            <img className="logo-navbar" src="logo swapp.webp" />
-          </a>
+            <img className="logo-navbar" src="/logo%20swapp.webp" alt="Swapp" />
+          </Link>
 
+          {/* Barra de búsqueda */}
           <form className="d-flex flex-grow-1 mx-3" role="search">
             <input
               className="form-control me-2 w-100"
@@ -24,17 +46,45 @@ function Navbar() {
               <i className="fa-solid fa-magnifying-glass"></i>
             </button>
           </form>
-          {/* <h5>Swapp, donde todo tiene otro valor.</h5> */}
+
+          {/* Botones de sesión */}
           <div className="d-flex">
-            <Link to="/registro" className="btn btn-main1 me-2" type="submit">
-              Regístrate
-            </Link>
-            <button className="btn btn-main1" type="submit">
-              Inicia sesión
-            </button>
+            {usuario ? (
+              <>
+                <Link
+                  to="/perfil"
+                  className="btn btn-main2 me-2"
+                  type="button"
+                >
+                  Perfil
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="btn btn-main1"
+                  type="button"
+                >
+                  Cerrar sesión
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  to="/registro"
+                  className="btn btn-main1 me-2"
+                  type="button"
+                >
+                  Regístrate
+                </Link>
+                <Link to="/login" className="btn btn-main1" type="button">
+                  Iniciar sesión
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </nav>
+
+      {/* === Navbar secundaria === */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <button
@@ -62,77 +112,20 @@ function Navbar() {
                   Todas las categorías
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Educación y Tutorías
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Tecnología y Programación
-                    </a>
-                  </li>
-
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Música y Audio
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Negocios y Finanzas
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Entretenimiento y Cultura
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Dibujo y pintura
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Deporte y Bienestar
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Moda, Belleza y Cuidado Personal
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Hogar y Reparaciones
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Mascotas y Animales
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Viajes y Estilo de Vida
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Comunicación y Marketing
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Desarrollo Personal y Coaching
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Otros / Misceláneos
-                    </a>
-                  </li>
+                  <li><a className="dropdown-item" href="#">Educación y Tutorías</a></li>
+                  <li><a className="dropdown-item" href="#">Tecnología y Programación</a></li>
+                  <li><a className="dropdown-item" href="#">Música y Audio</a></li>
+                  <li><a className="dropdown-item" href="#">Negocios y Finanzas</a></li>
+                  <li><a className="dropdown-item" href="#">Entretenimiento y Cultura</a></li>
+                  <li><a className="dropdown-item" href="#">Dibujo y pintura</a></li>
+                  <li><a className="dropdown-item" href="#">Deporte y Bienestar</a></li>
+                  <li><a className="dropdown-item" href="#">Moda, Belleza y Cuidado Personal</a></li>
+                  <li><a className="dropdown-item" href="#">Hogar y Reparaciones</a></li>
+                  <li><a className="dropdown-item" href="#">Mascotas y Animales</a></li>
+                  <li><a className="dropdown-item" href="#">Viajes y Estilo de Vida</a></li>
+                  <li><a className="dropdown-item" href="#">Comunicación y Marketing</a></li>
+                  <li><a className="dropdown-item" href="#">Desarrollo Personal y Coaching</a></li>
+                  <li><a className="dropdown-item" href="#">Otros / Misceláneos</a></li>
                 </ul>
               </li>
               <li className="nav-item">
@@ -140,27 +133,10 @@ function Navbar() {
                   Educación y Tutorías
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Tecnología y Programación
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Música y Audio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Negocios y Finanzas
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Entretenimiento y Cultura
-                </a>
-              </li>
+              <li className="nav-item"><a className="nav-link" href="#">Tecnología y Programación</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Música y Audio</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Negocios y Finanzas</a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Entretenimiento y Cultura</a></li>
             </ul>
           </div>
         </div>
