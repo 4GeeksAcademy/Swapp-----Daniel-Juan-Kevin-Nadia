b91174e393b062c037cf67b72b8b35c007e7e4a8
Nadia-2025 <nadyah-88@hotmail.com>
2025-10-19 08:01:48 +0000

    Hacer el Footer responsive

--- front/assets/components/Footer.jsx
+++ front/assets/components/Footer.jsx
@@ -5,10 +5,24 @@ function Footer() {
     <>
       <div className="container-fluid mt-5">
         <div className="row links-footer">
-          <div className="col-2  logo-container-footer">
-            <img className="logo-footer" src="logo swapp.webp"></img>
+          <div className="row d-flex justify-content-center d-lg-none">
+            <div className="col-12 logo-container-footer ">
+              <img
+                className="logo-footer "
+                src="logo swapp.webp"
+                alt="logo"
+              ></img>
+            </div>
           </div>
-          <div className="col-3">
+          <div className=" col-md-2 d-none d-lg-flex logo-container-footer ">
+            <img
+              className="logo-footer img-fluid"
+              src="logo swapp.webp"
+              alt="logo"
+            ></img>
+          </div>
+
+          <div className="col-6 col-md-3">
             <h5>Swapp</h5>
             <ul className="list-unstyled list">
               <li>
@@ -33,7 +47,7 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="col-3">
+          <div className="col-6 col-md-3">
             <h5>Soporte</h5>
             <ul className="list-unstyled list">
               <li>
@@ -53,7 +67,7 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="col-3">
+          <div className="col-6 col-md-3">
             <h5>Legal</h5>
             <ul className="list-unstyled list ">
               <li>
@@ -73,7 +87,7 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="col-1 text-start">
+          <div className="col-6 col-md-1 text-start">
             <h5>Síguenos</h5>
             <div className=" d-flex justify-content-start">
               <i className="fa-brands fa-facebook mt-3 me-2 fs-3"></i>
