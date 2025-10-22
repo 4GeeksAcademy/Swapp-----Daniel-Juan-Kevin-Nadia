7b75f4e0f9dd42a38e3c6ea1a6de8f0eb5f6fd8f
Kevinero25 <kevin-erazoromero@hotmail.com>
2025-10-22 11:06:31 +0000

    codigo error corregido

--- api/urls/categorias.py
+++ api/urls/categorias.py
@@ -1,5 +1,5 @@
 
-
+from sqlalchemy.exc import IntegrityError
 from flask import Blueprint, jsonify, request
 from models import db, Categoria
 
@@ -12,6 +12,8 @@ def obtener_categorias():
         Obtener Categorias
     """
     categorias_todas = Categoria.query.all()
+    if not categorias_todas:
+        return jsonify({"Error": "No hay categorias registradas"}), 404
     return jsonify([c.to_dict() for c in categorias_todas]), 200
 
 
@@ -21,18 +23,26 @@ def obtener_categoria(id_categoria):
         Obtener Categoria
     """
     categoria = Categoria.query.get_or_404(id_categoria)
-    return jsonify(categoria.to_dict()), 200
+    return jsonify(categoria.to_dict())
 
 
 @categorias.route('/api/categorias', methods=['POST'])
 def crear_categoria():
+    """
+        Crear Categoria
+    """
     data = request.get_json()
-
+    if not data or not data.get("nombre_categoria"):
+        return jsonify({"error": "El campo 'nombre_categoria' "}), 400
     nombre_categoria = data.get("nombre_categoria")
-
     nueva_categoria = Categoria(nombre_categoria=nombre_categoria)
     db.session.add(nueva_categoria)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback() 
+        return jsonify({"Error": "La categoria ya existe"}), 400
+
     return jsonify({"mensaje": "Categoría creada con éxito",
                     "categoria": nueva_categoria.to_dict()}), 201
 
@@ -54,7 +64,7 @@ def actualizar_categoria(id_categoria):
         "id_categoria": actualizar.id_categoria,
         "nombre_categoria": actualizar.nombre_categoria,
 
-    }), 200
+    })
 
 
 @categorias.route('/api/categorias/<int:id_categoria>', methods=['DELETE'])
@@ -67,4 +77,4 @@ def eliminar_categoria(id_categoria):
     db.session.commit()
     return jsonify({
         "Mensaje": "Categoria eliminada"
-    }), 200
+    })
