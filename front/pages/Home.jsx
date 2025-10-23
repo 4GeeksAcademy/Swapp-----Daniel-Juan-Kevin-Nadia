bb0262e6d2c1b99ef59ba108d5866eb70576d740
Daniel-And <danielanduezato@hotmail.com>
2025-10-23 19:17:45 +0000

    Agrego boton flotante y modal de mensajes, mejoras en vista perfiles

--- front/pages/Home.jsx
+++ front/pages/Home.jsx
@@ -2,15 +2,32 @@ import Navbar from "../assets/components/Navbar";
 import Carousel from "../assets/components/Carousel";
 import CardUsuario from "../assets/components/CardUsuario";
 import Footer from "../assets/components/Footer";
+import ModalMensajeria from "../assets/components/ModalMensajeria";
+import BotonMensajeria from "../assets/components/BotonMensajeria";
+import React, { useState } from "react";
 
 
 function Home() {
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
+
   return (
     <>
       <Navbar></Navbar>
       <Carousel></Carousel>
       <CardUsuario></CardUsuario>
       <Footer></Footer>
+
+      {usuario ? (
+        <>
+          <BotonMensajeria onClick={() => setMostrarModal(true)}></BotonMensajeria>
+          <ModalMensajeria
+            mostrar={mostrarModal}
+            cerrar={() => setMostrarModal(false)}>
+          </ModalMensajeria>
+        </>
+      ) : null}
+
     </>
   );
 }
