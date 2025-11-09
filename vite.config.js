39a2c1384e53d123b91b8318c6c0f799849c8f47
Kevinero25 <kevin-erazoromero@hotmail.com>
2025-11-09 23:09:26 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "front",
+  root: ".",                 // busca index.html en la raíz del proyecto
   build: {
-    outDir: "../dist",   // <-- clave: deja el build en la raíz del repo
+    outDir: "dist",          // deja el build en /dist (como Flask espera)
   },
   server: {
     port: 3000,
